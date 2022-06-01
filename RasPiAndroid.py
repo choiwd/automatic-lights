7e0d5dff084be971943e598e9f8ef7cc9da5d6cc
beatrizgsoares <beatrizgsoares97@gmail.com>
2022-06-01 19:02:28 +0100

    Código RaspPi para correr junto com app Android

--- RasPiAndroid.py
+++ RasPiAndroid.py
@@ -3,23 +3,21 @@ import logging
 import automaticlights_pb2
 import automaticlights_pb2_grpc
 from concurrent import futures
+import time
 
 
 count=0
 OnOff=False
 intensity=1
-participants=[]
-
+usersInRoom={}
+usersTimer={}
 
 class AutomaticLightsServicer(automaticlights_pb2_grpc.AutomaticLightsServicer):
 
     def TurnOnOff(self, request, context):
         global OnOff
         global intensity
-        global participants
-        if len(participants)>0:
-            voteID=1
-        elif request.OnOff==True:
+        if request.OnOff==True:
             if intensity==0:
                 voteID=1
             else:
@@ -34,11 +32,18 @@ class AutomaticLightsServicer(automaticlights_pb2_grpc.AutomaticLightsServicer):
     def status(self, request, context):
         global OnOff
         global intensity
-        global participants
+        global usersInRoom
+        global usersTimer
         print('status')
         userid=context.peer()
-        print(context.peer())
-        return automaticlights_pb2.queryMessage(OnOff=OnOff, intensity=intensity, voteID=0, participants=participants)
+        username=request.name
+        if userid not in usersInRoom:
+            usersInRoom[userid]=username
+            print('Boas vindas '+username)
+        usersTimer[userid]=time.time()
+
+        participants=list(usersInRoom.values())
+        return automaticlights_pb2.queryMessage(OnOff=OnOff, intensity=intensity, voteID=0, participants=participants, name=username)
 
 
 def serve():
@@ -47,29 +52,17 @@ def serve():
         AutomaticLightsServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
-    server.wait_for_termination()
+    #server.wait_for_termination()
 
 if __name__ == '__main__':
     logging.basicConfig()
     serve()
 
-""" example=AutomaticLightsServicer()
-request1=automaticlights_pb2.requestMessage(OnOff=True, voteID=1)
-request2=automaticlights_pb2.requestMessage(OnOff=False, voteID=1)
+    while True:
+        timerKeys=list(usersTimer.keys())
+        for key in timerKeys:
+            passedTime=time.time()-usersTimer[key]
+            if passedTime>10:
+                usersTimer.pop(key)
+                usersInRoom.pop(key)
 
-example.TurnOnOff(request1,2)
-state1=example.status(1,2)
-intensity=1
-example.TurnOnOff(request1,2)
-state2=example.status(1,2)
-example.TurnOnOff(request2,2)
-state3=example.status(1,2)
-intensity=0
-participants=['Joaquim','Joaquina']
-example.TurnOnOff(request1,2)
-state4=example.status(1,2)
-
-print(state1.OnOff,state1.intensity,state1.participants)
-print(state2.OnOff,state2.intensity,state2.participants)
-print(state3.OnOff,state3.intensity,state3.participants)
-print(state4.OnOff,state4.intensity,state4.participants)  """
