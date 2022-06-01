7ae4a71bcb4288f81ae8af6e94b049c6be44e074
beatrizgsoares <beatrizgsoares97@gmail.com>
2022-06-01 16:19:21 +0100

    gRPC

--- automaticlights_pb2.py
+++ automaticlights_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x15\x61utomaticlights.proto\x12\nautoLights\"/\n\x0erequestMessage\x12\r\n\x05OnOff\x18\x01 \x01(\x08\x12\x0e\n\x06voteID\x18\x03 \x01(\x05\"V\n\x0cqueryMessage\x12\r\n\x05OnOff\x18\x01 \x01(\x08\x12\x11\n\tintensity\x18\x02 \x01(\x05\x12\x0e\n\x06voteID\x18\x03 \x01(\x05\x12\x14\n\x0cparticipants\x18\x04 \x03(\t2\x98\x01\n\x0f\x41utomaticLights\x12\x45\n\tTurnOnOff\x12\x1a.autoLights.requestMessage\x1a\x1a.autoLights.requestMessage\"\x00\x12>\n\x06status\x12\x18.autoLights.queryMessage\x1a\x18.autoLights.queryMessage\"\x00\x42 \n\x02seB\x0f\x61utomaticLightsP\x01\xa2\x02\x06\x61utltsb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x15\x61utomaticlights.proto\x12\nautoLights\"/\n\x0erequestMessage\x12\r\n\x05OnOff\x18\x01 \x01(\x08\x12\x0e\n\x06voteID\x18\x03 \x01(\x05\"d\n\x0cqueryMessage\x12\r\n\x05OnOff\x18\x01 \x01(\x08\x12\x11\n\tintensity\x18\x02 \x01(\x05\x12\x0e\n\x06voteID\x18\x03 \x01(\x05\x12\x14\n\x0cparticipants\x18\x04 \x03(\t\x12\x0c\n\x04name\x18\x05 \x01(\t2\x98\x01\n\x0f\x41utomaticLights\x12\x45\n\tTurnOnOff\x12\x1a.autoLights.requestMessage\x1a\x1a.autoLights.requestMessage\"\x00\x12>\n\x06status\x12\x18.autoLights.queryMessage\x1a\x18.autoLights.queryMessage\"\x00\x42 \n\x02seB\x0f\x61utomaticLightsP\x01\xa2\x02\x06\x61utltsb\x06proto3')
 
 
 
@@ -42,7 +42,7 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _REQUESTMESSAGE._serialized_start=37
   _REQUESTMESSAGE._serialized_end=84
   _QUERYMESSAGE._serialized_start=86
-  _QUERYMESSAGE._serialized_end=172
-  _AUTOMATICLIGHTS._serialized_start=175
-  _AUTOMATICLIGHTS._serialized_end=327
+  _QUERYMESSAGE._serialized_end=186
+  _AUTOMATICLIGHTS._serialized_start=189
+  _AUTOMATICLIGHTS._serialized_end=341
 # @@protoc_insertion_point(module_scope)
